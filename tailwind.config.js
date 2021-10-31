36a79d4165386b41a35a07c59d9b592b148d386d
Radoy Stoyanov <radoy.stoyanov@gmail.com>
2021-10-31 19:09:11 +0200

    Complete rework of the landing page with new content and using Tailwind CSS

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {    
+    extend: {
+      colors: {
+        'teal-lighter': '#A9F3EA',
+        'teal-light': '#6AE7DA',
+        'teal': '#18D0BA',
+        'teal-dark': '#0CA896',
+        'teal-darker': '#0E6459',
+      },
+      fontFamily: {
+        rubik: "'Rubik', sans-serif",
+      },
+      fontWeight: {
+        light: 300,
+        medium: 500,
+        bold: 700
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
